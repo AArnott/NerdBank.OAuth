4e30b2ef65043eb09c48e6c63631bce34c28f125
Andrew Arnott <andrewarnott@gmail.com>
2013-12-09 22:27:39 -0800

    Adds test project.

---
+++ NerdBank.OAuth.Tests/OAuth1ConsumerBaseTests.cs
@@ -0,0 +1,21 @@
+﻿namespace NerdBank.OAuth.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Xunit;
+
+	public class OAuth1ConsumerBaseTests {
+		[Fact]
+		public void DefaultCtor() {
+			var consumer = new MyDerived();
+		}
+
+		private class MyDerived : OAuth1ConsumerBase {
+			protected override byte[] ComputeHmacSha1(byte[] data, byte[] key) {
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
