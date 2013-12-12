a4629c30d533840b46275d812ce86c0c8d46e9ac
Andrew Arnott <andrewarnott@gmail.com>
2013-12-12 07:51:02 -0800

    Build of solution and nuget package now succeeds.

--- NerdBank.OAuth/Properties/AssemblyInfo.cs
+++ NerdBank.OAuth/Properties/AssemblyInfo.cs
@@ -3,11 +3,4 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NerdBank.OAuth")]
 [assembly: AssemblyDescription("OAuth 1.0 Consumer portable class library targeting .NET Framework 4.5, Windows Phone 8 and Windows 8.")]
-[assembly: AssemblyProduct("NerdBank.OAuth")]
-[assembly: AssemblyCopyright("Copyright © 2013 Andrew Arnott")]
-[assembly: NeutralResourcesLanguage("en")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
