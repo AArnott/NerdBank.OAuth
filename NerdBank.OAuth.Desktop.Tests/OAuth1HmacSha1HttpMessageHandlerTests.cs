96ee3e4433f851d4ce00518ec2b40112bf9b7ee8
Andrew Arnott <andrewarnott@gmail.com>
2013-12-14 09:05:31 -0800

    Adds the first unit tests for the message handlers.

---
+++ NerdBank.OAuth.Desktop.Tests/OAuth1HmacSha1HttpMessageHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿namespace NerdBank.OAuth.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+#if DESKTOP
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+	using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#endif
+
+	[TestClass]
+	public class OAuth1HmacSha1HttpMessageHandlerTests {
+		[TestMethod]
+		public void SignatureMethod() {
+			var derived = new OAuth1HmacSha1HttpMessageHandlerDerived();
+			Assert.AreEqual("HMAC-SHA1", derived.SignatureMethod);
+		}
+
+		private class OAuth1HmacSha1HttpMessageHandlerDerived : OAuth1HmacSha1HttpMessageHandler {
+			internal new string SignatureMethod {
+				get { return base.SignatureMethod; }
+			}
+		}
+	}
+}
---
+++ NerdBank.OAuth.Desktop.Tests/OAuth1PlainTextMessageHandlerTests.cs
@@ -0,0 +1,27 @@
+﻿namespace NerdBank.OAuth.Tests {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+#if DESKTOP
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+#else
+	using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#endif
+
+	[TestClass]
+	public class OAuth1PlainTextMessageHandlerTests {
+		[TestMethod]
+		public void SignatureMethod() {
+			var derived = new OAuth1PlainTextMessageHandlerDerived();
+			Assert.AreEqual("PLAINTEXT", derived.SignatureMethod);
+		}
+
+		private class OAuth1PlainTextMessageHandlerDerived : OAuth1PlainTextMessageHandler {
+			internal new string SignatureMethod {
+				get { return base.SignatureMethod; }
+			}
+		}
+	}
+}
